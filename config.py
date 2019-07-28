000a20326128dc91f40d11b27464ea79e76453fd
SoSIlent <1325360514@qq.com>
2019-07-28 17:49:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,8 @@ DEBUG = True
 
 #Nick Name
 NICKNAME = {'Jarvis','jarvis','JARVIS'}
-SUPERUSERS = {1325360514}
+SUPERUSER = {}
 COMMAND_START = {'/', '!', '／', '！',''}
-#聚合数据KEY
-JUHE_JOKE_API_KEY = None
 
 #和风天气KEY
-HE_WEATHER_KEY = '2d849c62d67a4b9e94607d0f1c744561'
+HE_WEATHER_KEY = None
